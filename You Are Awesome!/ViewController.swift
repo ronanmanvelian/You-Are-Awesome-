6e426e26276b977cbae0495d85494bd97de266ec
Ronan Manvelian <manvelia@bc.edu>
2020-01-20 15:41:24 -0500

    Completed 2.4 Challenge

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -18,11 +18,13 @@ class ViewController: UIViewController {
     @IBAction func showMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
         messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = NSTextAlignment.left
     }
     
     @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You Are Great!"
         messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = NSTextAlignment.right
     }
     
         
